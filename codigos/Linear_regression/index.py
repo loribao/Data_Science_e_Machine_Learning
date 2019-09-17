dcd6640d649912ca3c567909be451d7924a4a488
loribao <lorival.sanjinez@hotmail.com>
2019-09-17 20:56:11 -0300

    Classificando por arredondamento

--- codigos/Linear_regression/index.py
+++ codigos/Linear_regression/index.py
@@ -1,3 +1,6 @@
+#Objetivo: Aplicar diversos algortimos no dataset iris. Assim conhecer e interpretar os algortmos.
+#
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,16 +29,28 @@ y = y.astype(float)
 x_treino,x_teste,y_treino,y_teste = train_test_split(x,y,test_size=0.3)
 # instanciação da classe de regressão
 from sklearn.linear_model import LinearRegression
+
 lm = LinearRegression()
+
 #geração do modelo/treino
+
 lm.fit(x_treino,y_treino)
 
 predict = lm.predict(x_teste)
-
+Classificar =  np.array([np.around(x) for x in predict])
 sbs.distplot((y_teste - predict))
 plt.show()
 
-print('intercept_: \n')
+from sklearn.metrics import mean_squared_error
+
+print('Predict Mean Squared Error:')
+print(mean_squared_error(y_teste,predict))
+print('\nintercept_: ')
 print(lm.intercept_)
-print('coef_: \n')
+print('\ncoef_: ')
 print(lm.coef_)
+print('\n---------------Resultado final----------------------\n')
+print('Y\tNao Classificado ->\t\tClassifido\n')
+for i in range(0,45):    
+    print('{}\t{} -------> {}'.format(y_teste.iloc[i].values,predict[i],Classificar[i]))
+
